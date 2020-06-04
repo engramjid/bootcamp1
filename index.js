34c140469054275d59b785ec1a2c3186a6cd55c6
engramjid <58560284+engramjid@users.noreply.github.com>
2020-06-04 16:45:50 +0500

    updated index.html
    jascript and index updated

---
+++ index.js
@@ -0,0 +1,7 @@
+<h1>Temperature converter</h1>
+var CD = 25;
+var FD = 70;
+C = (FD-32)*5/9;
+F = (CD*(9/5))+32;
+document.write(CD+"<sup>o</sup>C is: "+F+"<sup>o</sup>F<br>");
+document.write(FD+"<sup>o</sup>F is: "+C+"<sup>o</sup>C<br>");
